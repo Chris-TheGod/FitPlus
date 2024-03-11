876f0223eeb2a59f8de49c92740cdf48e4e71806
Kristjan Laane <xonproject@gmail.com>
2024-03-11 22:48:43 +0200

    navbar2

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import {
   AppBar,
+  Avatar,
   Badge,
   Box,
   InputBase,
@@ -8,10 +9,11 @@ import {
   styled,
 } from "@mui/material";
 import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
-import { Mail } from "@mui/icons-material";
+import { Mail, Notifications } from "@mui/icons-material";
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
+
   justifyContent: "space-between",
 });
 
@@ -22,16 +24,37 @@ const Search = styled("div")(({ theme }) => ({
   width: "40%",
 }));
 
-const Icons = styled(Box)(({ theme }) => ({}));
+const Icons = styled(Box)(({ theme }) => ({
+  display: "none",
+  alignItems: "center",
+  gap: "20px",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
 
 const Navbar = () => {
   return (
     <AppBar>
       <StyledToolbar>
-        <Typography variant='h6' sx={{ display: { xs: "none", sm: "block" } }}>
-          FitPlus
-        </Typography>
-        <FitnessCenterIcon sx={{ display: { xs: "block", sm: "none" } }} />
+        <Box display='flex' alignItems='center' gap='10px'>
+          <FitnessCenterIcon />
+          <Typography
+            variant='h6'
+            sx={{ display: { xs: "none", sm: "block" } }}
+          >
+            FitPlus
+          </Typography>
+        </Box>
         <Search>
           <InputBase placeholder='search...' />
         </Search>
@@ -39,7 +62,15 @@ const Navbar = () => {
           <Badge badgeContent={4} color='error'>
             <Mail />
           </Badge>
+          <Badge badgeContent={2} color='error'>
+            <Notifications />
+          </Badge>
+          <Avatar sx={{ width: 30, height: 30 }} />
         </Icons>
+        <UserBox>
+          <Avatar sx={{ width: 30, height: 30 }} />
+          <Typography variant='span'>Chris</Typography>
+        </UserBox>
       </StyledToolbar>
     </AppBar>
   );
