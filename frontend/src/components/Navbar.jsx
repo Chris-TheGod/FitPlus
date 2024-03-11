de1616c0fc7ef9c819b2ff88aaa0eba971b6a1df
Kristjan Laane <xonproject@gmail.com>
2024-03-11 22:34:09 +0200

    navbar start

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,28 @@
-import { AppBar, Toolbar, Typography, styled } from "@mui/material";
+import {
+  AppBar,
+  Badge,
+  Box,
+  InputBase,
+  Toolbar,
+  Typography,
+  styled,
+} from "@mui/material";
 import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import { Mail } from "@mui/icons-material";
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
   justifyContent: "space-between",
 });
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({}));
 
 const Navbar = () => {
   return (
@@ -14,6 +32,14 @@ const Navbar = () => {
           FitPlus
         </Typography>
         <FitnessCenterIcon sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder='search...' />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color='error'>
+            <Mail />
+          </Badge>
+        </Icons>
       </StyledToolbar>
     </AppBar>
   );
