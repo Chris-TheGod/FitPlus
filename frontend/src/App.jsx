fbcd659e1eda3922823055959722bb52dbc32d5d
Kristjan Laane <xonproject@gmail.com>
2024-03-11 23:27:17 +0200

    layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Rightbar from "./components/Rightbar";
+import Feed from "./components/Feed";
+import { Box, Stack } from "@mui/material";
 
 function App() {
   return (
-    <div>
+    <Box>
       <Navbar />
-    </div>
+      <Stack
+        direction='row'
+        marginTop='60px'
+        spacing={2}
+        justifyContent='space-between'
+      >
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
   );
 }
 
---
+++ frontend/src/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    main: "black",
+    light: "black",
+  },
+  secondary: {
+    main: "#15c630",
+  },
+});
